29d068af405330d8a7778acfe52c9aae038dc6bd
Rafael Freitas <rafael.freitasfa@gmail.com>
2024-11-08 11:09:33 -0300

    fix: Fix upsert transaction function

--- app/_actions/upsert-transaction/index.ts
+++ app/_actions/upsert-transaction/index.ts
@@ -29,11 +29,11 @@ export const upsertTransaction = async (params: UpsertTransactionParams) => {
   }
 
   await db.transaction.upsert({
+    update: { ...params, userId },
+    create: { ...params, userId },
     where: {
       id: params.id ?? "",
     },
-    update: { ...params, userId },
-    create: { ...params, userId },
   });
 
   revalidatePath("/transactions");
