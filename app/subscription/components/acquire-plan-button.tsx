f8da80588dfae63810ccbb3339ea3a2faa314b68
Rafael Freitas <rafael.freitasfa@gmail.com>
2024-11-14 15:31:48 -0300

    fix: Rewriting create stripe checkout import

--- app/subscription/components/acquire-plan-button.tsx
+++ app/subscription/components/acquire-plan-button.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Button } from "@/app/_components/ui/button";
 
-import { createStripeCheckout } from "../_actions/create-checkout";
+import { createStripeCheckout } from "../_actions/create-stripe-checkout";
 
 import { loadStripe } from "@stripe/stripe-js";
 import { useUser } from "@clerk/nextjs";
