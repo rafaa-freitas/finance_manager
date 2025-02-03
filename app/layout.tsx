97aeb4e9547385f8a3e8087e81bad359005493bb
Rafael Freitas <rafael.freitasfa@gmail.com>
2025-02-03 19:35:46 -0300

    refactor: Changing lang

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ptBR">
       <body className={`${mulish.className} dark antialiased`}>
         <ClerkProvider
           appearance={{
